43369c4a7230bd41782b45f3e4c61a0dad4b316a
manas-sahu <manas.sahu.one@gmail.com>
2021-04-11 13:34:20 +0530

    Final Code

--- js/main.js
+++ js/main.js
@@ -1,2 +1,58 @@
+let toDolists = [];
+let userData;
 const inputBox = document.querySelector(".inputField input");
+const addButton = document.querySelector(".inputField button");
+const htmlToDoList = document.querySelector(".todoList");
+const totalTasks = document.querySelector(".footer .totalRecords")
 console.log(inputBox);
+console.log(totalTasks.textContent);
+
+inputBox.onkeyup = () => {
+    userData = inputBox.value.trim();
+}
+checkExsiting();
+
+function addDataToList() {
+    if (userData.length != 0) {
+        toDolists.push(userData);
+        console.log(toDolists);
+        inputBox.value = "";
+        setItem();
+    }
+    showList();
+}
+
+function showList() {
+    htmlToDoList.innerHTML = "";
+    toDoLists = getItem();
+    toDolists.forEach((value, index) => {
+        htmlToDoList.innerHTML += `<li>${value}<span><i onclick="removeItem(${index})" class="fas fa-trash"></i></span></li>`
+    });
+    totalTasks.textContent = toDolists.length; 
+}
+function checkExsiting() {
+    let oldRecord = getItem();
+    if(oldRecord.length > 0) {
+        toDolists = oldRecord;
+        showList();
+    }
+ }
+ function removeItem(index) {
+     toDolists.splice(index, 1);
+     setItem();
+     showList();
+ }
+
+ function setItem() {
+    localStorage.setItem("allList", JSON.stringify(toDolists));
+ }
+
+ function getItem() {
+    return JSON.parse(localStorage.getItem("allList"));
+ }
+
+ function removeAllItem() {
+     toDolists = [];
+     setItem();
+     showList();
+ }
